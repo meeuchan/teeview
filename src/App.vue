570168ea5b37f78370047a41e8ca1410776c5d6a
Meeu <me@meeu.me>
2024-12-16 21:58:32 +0700

    Add title and buttons

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import QuestionIcon from './components/icons/QuestionIcon.vue'
+import SunIcon from './components/icons/SunIcon.vue'
+</script>
 
 <template>
-  <h1>Hello, world!</h1>
+  <div class="app-container">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col">
+          <h1>TeeView</h1>
+        </div>
+        <div class="col d-flex justify-content-end align-items-center">
+          <button type="button" class="btn btn-warning btn-sm rounded-pill ms-2">
+            <SunIcon />
+          </button>
+          <button type="button" class="btn btn-primary btn-sm rounded-pill ms-2">
+            <QuestionIcon />
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
