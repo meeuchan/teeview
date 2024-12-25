8c86fd6d7430b5187d543f2f96487f102edb8ff0
Meeu <me@meeu.me>
2024-12-26 06:50:07 +0700

    Create simple tab system

---
+++ src/components/tabs/Tab.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+import { ActiveTabKey } from './TabSymbols'
+
+const props = defineProps<{
+  title: string
+  disabled?: boolean
+}>()
+
+const activeTabKey = inject(ActiveTabKey)
+</script>
+
+<template>
+  <div v-show="props.title === activeTabKey">
+    <slot></slot>
+  </div>
+</template>
