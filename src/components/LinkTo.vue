011e54803143d7c0554a58d4112e86a1d1f3ef39
Meeu <me@meeu.me>
2024-12-26 11:49:47 +0700

    Create LinkTo component

---
+++ src/components/LinkTo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  href: string
+}>()
+</script>
+
+<template>
+  <a
+    :href="props.href"
+    target="_blank"
+    class="link-underline link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"
+    ><slot></slot
+  ></a>
+</template>
