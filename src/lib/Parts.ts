4ee96f8730b6b2ef1694bd2d0f4f07bef71e05d3
Meeu <me@meeu.me>
2024-12-26 16:58:11 +0700

    Create skin class

---
+++ src/lib/Parts.ts
@@ -0,0 +1,23 @@
+export enum PartType {
+  Body = 'body',
+  BodyShadow = 'bodyShadow',
+  Foot = 'foot',
+  FootShadow = 'footShadow',
+  Eye = 'eye',
+}
+
+export enum EyeType {
+  Normal,
+  Angry,
+  Hurt,
+  Happy,
+  Ninja,
+  Surprised,
+  Sad,
+}
+
+export enum FaceType {
+  Right,
+  Left,
+  Front,
+}
---
+++ src/lib/Skin.ts
@@ -0,0 +1,80 @@
+import Canvas from './Canvas'
+import { EyeType, PartType } from './Parts'
+
+export class Skin {
+  private _img: HTMLImageElement
+  private _cache: Record<string, HTMLCanvasElement> = {}
+
+  private _scale: number
+  public get scale() {
+    return this._scale
+  }
+
+  constructor(img: HTMLImageElement) {
+    this._img = img
+    this._scale = img.width / 256
+  }
+
+  public getBody() {
+    if (!this._cache[PartType.Body]) {
+      this._cache[PartType.Body] = this._getPart(96, 96, 0, 0)
+    }
+    return this._cache[PartType.Body]
+  }
+
+  public getBodyShadow() {
+    if (!this._cache[PartType.BodyShadow]) {
+      this._cache[PartType.BodyShadow] = this._getPart(96, 96, 96, 0)
+    }
+    return this._cache[PartType.BodyShadow]
+  }
+
+  public getFoot() {
+    if (!this._cache[PartType.Foot]) {
+      this._cache[PartType.Foot] = this._getPart(64, 32, 192, 32)
+    }
+    return this._cache[PartType.Foot]
+  }
+
+  public getFootShadow() {
+    if (!this._cache[PartType.FootShadow]) {
+      this._cache[PartType.FootShadow] = this._getPart(64, 32, 192, 64)
+    }
+    return this._cache[PartType.FootShadow]
+  }
+
+  public getEye(eyes: EyeType) {
+    const key = PartType.Eye + eyes
+    if (!this._cache[key]) {
+      const shouldFlip = eyes === EyeType.Sad
+      if (EyeType.Sad) eyes = EyeType.Angry
+
+      const eyeCanvas = this._getPart(32, 32, 64 + 32 * eyes, 96)
+      this._cache[key] = shouldFlip ? Canvas.flip(eyeCanvas) : eyeCanvas
+    }
+    return this._cache[key]
+  }
+
+  private _getPart(
+    partWidth: number,
+    partHeight: number,
+    partOffsetX: number,
+    partOffsetY: number,
+  ) {
+    const { canvas, ctx } = Canvas.create(partWidth * this._scale, partHeight * this._scale)
+
+    ctx.drawImage(
+      this._img,
+      partOffsetX * this._scale,
+      partOffsetY * this._scale,
+      canvas.width,
+      canvas.height,
+      0,
+      0,
+      canvas.width,
+      canvas.height,
+    )
+
+    return canvas
+  }
+}
