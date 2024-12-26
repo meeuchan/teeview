1265a2b7d46fed86a5c87d509ed51039ea968513
Meeu <me@meeu.me>
2024-12-26 19:04:56 +0700

    Pass options to TeeView component and render result to canvas

--- src/components/TeeOptions.vue
+++ src/components/TeeOptions.vue
@@ -3,20 +3,34 @@ import { ref } from 'vue'
 import Tab from './tabs/Tab.vue'
 import Tabs from './tabs/Tabs.vue'
 import TeeOptionsSkin from './TeeOptionsSkin.vue'
+import type { IRendererOptions } from '@/lib/Renderer'
 
 const skin = ref<HTMLImageElement>()
+
+const emit = defineEmits<{
+  change: [value: IRendererOptions]
+}>()
+
+function buildOptions() {
+  if (!skin.value) return
+
+  const options: IRendererOptions = {
+    skin: skin.value,
+  }
+  emit('change', options)
+}
 </script>
 
 <template>
   <div id="teeOptions" class="card">
     <Tabs>
       <Tab title="Skin" class="card-body">
-        <TeeOptionsSkin @change="skin = $event" />
+        <TeeOptionsSkin @change="((skin = $event), buildOptions())" />
       </Tab>
-      <Tab title="Color" class="card-body">
+      <Tab title="Color" :disabled="!skin" class="card-body">
         <div>B</div>
       </Tab>
-      <Tab title="Options" disabled class="card-body"></Tab>
+      <Tab title="Options" :disabled="!skin" class="card-body"></Tab>
     </Tabs>
   </div>
 </template>
--- src/components/TeeView.vue
+++ src/components/TeeView.vue
@@ -1,9 +1,51 @@
+<script setup lang="ts">
+import { defineProps, onMounted, ref, useTemplateRef, watch } from 'vue'
+import { Renderer, type IRendererOptions } from '@/lib/Renderer'
+
+const props = defineProps<{
+  options?: IRendererOptions
+}>()
+
+const renderer = ref(new Renderer())
+const canvas = useTemplateRef('teePreview')
+
+onMounted(() => {
+  if (props.options) renderTee(props.options)
+})
+
+watch(
+  () => props.options,
+  (newOptions) => {
+    if (!newOptions) return
+    renderTee(newOptions)
+  },
+)
+
+function renderTee(options: IRendererOptions) {
+  if (!canvas.value) return
+  const render = renderer.value.render(options)
+  if (!render) return
+  const ctx = canvas.value.getContext('2d')!
+
+  canvas.value.width = render.width
+  canvas.value.height = render.height
+  ctx.drawImage(render, 0, 0)
+}
+</script>
+
 <template>
-  <div id="teeView" class="card"></div>
+  <div id="teeView" class="card d-flex align-items-center justify-content-center">
+    <canvas id="teePreview" ref="teePreview"></canvas>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 #teeView {
   height: 256px;
+}
+
+#teePreview {
+  height: 192px;
+  width: 192px;
 }
 </style>
