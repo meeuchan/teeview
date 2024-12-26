163a3f15d7a45a536598a1033f735c538b1433d6
Meeu <me@meeu.me>
2024-12-26 12:27:38 +0700

    Create skin tab content

--- src/components/TeeOptions.vue
+++ src/components/TeeOptions.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Tab from './tabs/Tab.vue'
 import Tabs from './tabs/Tabs.vue'
+import TeeOptionsSkin from './TeeOptionsSkin.vue'
+
+const skin = ref<HTMLImageElement>()
 </script>
 
 <template>
   <div id="teeOptions" class="card">
     <Tabs>
       <Tab title="Skin" class="card-body">
-        <div>A</div>
+        <TeeOptionsSkin @change="skin = $event" />
       </Tab>
       <Tab title="Color" class="card-body">
         <div>B</div>
