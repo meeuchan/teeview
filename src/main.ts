6e670a6a2882dc8cf41f14e2da51553375990494
Meeu <me@meeu.me>
2026-09-01 17:28:46 -0400

    Fix PWA caching old code issue

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './assets/main.scss'
 
 import { createApp } from 'vue'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.vue'
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+registerSW({ immediate: true })
 
 createApp(App).mount('#app')
